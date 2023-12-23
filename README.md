1dd5859aa3656b724945140acfb8af1cc2152002
hvad <david.hannequin@gmail.com>
2023-12-23 10:43:26 +0100

    Add check configuration file.

--- README.md
+++ README.md
@@ -3,22 +3,32 @@
 
 ## Description
 
+Python tool to monitoring linux system. 
 
-## Feature
+## Features
+
+Check configuration file before start
 
 Check load average
+
 Check memory usage
+
 Check disk usage
+
 Check NTP drift/sync
+
 Check firewall change and specific rules
+
 Check SSL Certificate expired
 
 Stats disk I/O
+
 Stats network I/O
 
-
 Log
+
 Alert 
+
 Discovery disks
 
 
@@ -27,5 +37,39 @@ Discovery disks
 
 ## How to configure
 
+Configuration file example : 
+```
+[Settings]
+check_period=60
+logfile_name=std.log
+logfile_days=7
 
+[Load]
+enable=True
 
+[Disks]
+enable=True
+warning=80
+critical=90
+
+[Memory]
+warning=90
+enable=True
+critical=98
+
+[Network]
+enable=True
+name="lo"
+
+[Email]
+Sender = your_email@example.com
+Receiver = recipient_email@example.com
+SMTPServer = smtp.example.com
+SMTPPort = 587
+SMTPUsername = your_username
+SMTPPassword = your_password
+```
+
+```
+main.py -c config.ini 
+```
--- src/py-monitoring-sys/main.py
+++ src/py-monitoring-sys/main.py
@@ -19,34 +19,38 @@ from checks.network.check_network import get_network_interface_state
 
 async def sys_get_load_average():
     """ Get load average 1 minute, 5 minutes and 15 minutes."""
-    load = get_load_average()
-    logging.info(f"Load : {load}")
+    if check_load_enabled:
+        load = get_load_average()
+        logging.info(f"Load : {load}")
 
 async def sys_get_memory_usage(warning,critical):
     """ Get memory percent usage."""
-    memory = get_memory_usage()
-
-    if  memory >= int(critical):
-        logging.info(f"CRITICAL - Memory percentage usage : {memory}%")
-    elif memory >= int(warning):
-        logging.info(f"WARNING - Memory percentage usage : {memory}%")
-    else:
-        logging.info(f"OK - Memory percentage usage : {memory}%")
+    if check_memory_enabled:
+        memory = get_memory_usage()
+        if  memory >= int(critical):
+            logging.info(f"CRITICAL - Memory percentage usage : {memory}%")
+        elif memory >= int(warning):
+            logging.info(f"WARNING - Memory percentage usage : {memory}%")
+        else:
+            logging.info(f"OK - Memory percentage usage : {memory}%")
 
 async def sys_get_disk_usage(partition):
     """ Get disk usage."""
-    disk_usage_all = get_disk_usage(partition)
-    logging.info(f"Disk Usage : {disk_usage_all}")
+    if check_disk_enabled:
+        disk_usage_all = get_disk_usage(partition)
+        logging.info(f"Disk Usage : {disk_usage_all}")
 
 async def sys_get_disk_io():
     """ Get disk I/O."""
-    disk_io = get_disk_io()
-    logging.info(f"Disk I/O Bytes : {disk_io}")
+    if check_disk_enabled:
+        disk_io = get_disk_io()
+        logging.info(f"Disk I/O Bytes : {disk_io}")
 
 async def sys_get_network_interface_state(interface_name):
     """ Get network state."""
-    net = get_network_interface_state(interface_name)
-    logging.info(f"Network : {net}")
+    if check_network_enabled:
+        net = get_network_interface_state(interface_name)
+        logging.info(f"Network : {net}")
 
 async def run_checks():
     """ Run the checks asynchronously."""
@@ -61,6 +65,20 @@ async def run_checks():
         ]
         await asyncio.gather(*tasks)
         await asyncio.sleep(int(config['Settings']['check_period']))  # Add a small delay before running the checks again
+
+def validate_configuration(config):
+    required_sections = ["Settings", "Load"]
+    required_keys = {
+        "Settings": ["check_period", "logfile_name", "logfile_days"],
+        "Load": ["enable"]
+    }
+
+    for section in required_sections:
+        if section not in config:
+            raise ValueError(f"Missing required section: {section}")
+        for key in required_keys[section]:
+            if key not in config[section]:
+                raise ValueError(f"Missing required key: {key} in section: {section}")
 
 
 def main():
@@ -80,6 +98,18 @@ if __name__ == "__main__":
 
     config = configparser.ConfigParser()
     config.read(CONFIG_FILE)
+
+    # Validate the configuration before starting
+    try:
+        validate_configuration(config)
+    except ValueError as e:
+        print(f"Invalid configuration: {e}")
+        exit(1)
+
+    check_load_enabled = config.getboolean("Load","enable") 
+    check_disk_enabled = config.getboolean("Disks","enable") 
+    check_memory_enabled = config.getboolean("Memory","enable") 
+    check_network_enabled = config.getboolean("Network","enable") 
 
     if args.daemon:
         with daemon.DaemonContext(stderr=sys.stderr):
